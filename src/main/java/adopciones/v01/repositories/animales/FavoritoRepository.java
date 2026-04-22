a93c56b8d73aa29846b3d857a64d862e67e09c1b
Fabi <fabcositas@gmail.com>
2026-04-22 21:41:29 +0200

    creacion api favoritos funcional, y comentarios en otras clases

--- src/main/java/adopciones/v01/repositories/animales/FavoritoRepository.java
+++ src/main/java/adopciones/v01/repositories/animales/FavoritoRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface FavoritoRepository extends JpaRepository<FavoritoModel, Long> {
@@ -15,7 +14,6 @@ public interface FavoritoRepository extends JpaRepository<FavoritoModel, Long> {
     List<FavoritoModel> findByUsuario(UsuarioModel usuario);
     List<FavoritoModel> findByAnimal(AnimalitoModel animal);
 
-    Optional<FavoritoModel> findByUsuarioAndAnimal(UsuarioModel usuario, AnimalitoModel animal);
     boolean existsByUsuarioAndAnimal(UsuarioModel usuario, AnimalitoModel animal);
 
     void deleteByUsuarioAndAnimal(UsuarioModel usuario, AnimalitoModel animal);
